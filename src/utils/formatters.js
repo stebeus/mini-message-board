efec6dff55f1e475e42ebf8d92bad301191ec632
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-24 17:23:05 -0300

    feat: add formatToCamelCase

---
+++ src/utils/formatters.js
@@ -0,0 +1,8 @@
+export function formatToCamelCase(string) {
+  const lowerCased = string.toLowerCase();
+
+  const capitalizeRemainder = (string, character) =>
+    string + (character.charAt(0).toUpperCase() + character.slice(1));
+
+  return lowerCased.split(' ').reduce(capitalizeRemainder).replace(/\W/g, '');
+}
