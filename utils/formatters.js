e1d710d7e222abdb83df5c6be6c97248e4f27ec6
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-24 12:54:03 -0300

    chore!: restart project

--- utils/formatters.js
+++
@@ -1,8 +0,0 @@
-export function toCamelCase(string) {
-  const lowerCased = string.toLowerCase();
-
-  const capitalizeRemainder = (string, character) =>
-    string + (character.charAt(0).toUpperCase() + character.slice(1));
-
-  return lowerCased.split(' ').reduce(capitalizeRemainder).replace(/\W/g, '');
-}
