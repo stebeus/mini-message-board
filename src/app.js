26c276adeb5bf2c771bf504b496a1abd42758bb1
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-24 16:09:32 -0300

    feat: add path constants

--- src/app.js
+++ src/app.js
@@ -4,3 +4,6 @@ import express from 'express';
 export const app = express();
 
 const dirname = import.meta.dirname;
+
+const assetsPath = path.join(dirname, 'public');
+const viewsPath = path.join(dirname, 'views');
