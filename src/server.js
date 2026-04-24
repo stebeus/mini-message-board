a835c6319eae02ec8198b793c83c7fef5222a88d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-24 16:51:53 -0300

    feat: add error handler

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import { app } from './app.js';
+import { INTERNAL_SERVER_ERROR } from './constants.js';
 
 const PORT = 3000;
+
+app.use((error, _request, response, next) => {
+  if (response.headersSent) return next(error);
+
+  const statusCode = error.status ?? INTERNAL_SERVER_ERROR;
+
+  console.error(error.stack);
+  response.status(statusCode).render('error', { statusCode, error });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
