c63fa51f8de7e4bb54f26d81659f7a906c43cdef
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-24 17:18:19 -0300

    test: add unit for string camel casing

--- src/utils/formatters.test.js
+++ src/utils/formatters.test.js
@@ -1,3 +1,10 @@
-import { describe } from 'node:test';
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
 
-describe('formatToCamelCase', () => {});
+import { formatToCamelCase } from './formatters.js';
+
+describe('formatToCamelCase', () => {
+  it('camel cases strings', () => {
+    assert.equal(formatToCamelCase('Foo BaR 123'), 'fooBar123');
+  });
+});
