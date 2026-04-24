007b6a2f99e40f7a92b85b21a24d806ac2c2912d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-24 09:51:33 -0300

    test: add numbers to the assertion

--- utils/formatters.test.js
+++ utils/formatters.test.js
@@ -5,7 +5,7 @@ import { toCamelCase } from './formatters.js';
 
 describe('toCamelCase', () => {
   it('camel case strings', () => {
-    assert.equal(toCamelCase('Foo BaR'), 'fooBar');
+    assert.equal(toCamelCase('Foo BaR 123'), 'fooBar123');
   });
 
   it('removes non-alphanumeric characters', () => {
