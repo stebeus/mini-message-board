28bdda8c69d5fbd050b393f0e7901052fe3f2b7e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-24 17:09:52 -0300

    chore: add section comments

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ import { ASSETS_PATH, VIEWS_PATH } from './constants.js';
 
 export const app = express();
 
+// Setup
+
 app.use(express.static(ASSETS_PATH));
 app.locals.basedir = VIEWS_PATH;
 
 app.set('views', VIEWS_PATH);
 app.set('view engine', 'pug');
+
+// Routes
 
 app.get('/', (_request, response) => {
   response.render('index');
