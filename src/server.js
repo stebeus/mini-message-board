1e516477f06adb83d8d11ab2d06e43f19cfd8974
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-25 14:23:47 -0300

    feat!: remove error handlers

--- src/server.js
+++ src/server.js
@@ -1,22 +1,6 @@
 import { app } from './app.js';
-import { INTERNAL_SERVER_ERROR, NOT_FOUND } from './constants.js';
 
 const PORT = 3000;
-
-// 404 forwarder
-app.use((_request, _response, next) => {
-  next({ status: NOT_FOUND, message: 'Not found' });
-});
-
-// Error handler
-app.use((error, _request, response, next) => {
-  if (response.headersSent) return next(error);
-
-  const statusCode = error.status ?? INTERNAL_SERVER_ERROR;
-
-  console.error(error.stack);
-  response.status(statusCode).render('error', { statusCode, error });
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
