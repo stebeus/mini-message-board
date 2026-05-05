ceb06efa141d0d9a7ab7b9cfdca9eb50fb9c59ff
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-05 15:09:25 -0300

    chore!: restart project

--- README.md
+++
@@ -1,7 +0,0 @@
-# Mini Message Board
-
-Post messages to a simple board.
-
-## License
-
-[MIT](LICENSE)
--- src/utils/formatters.js
+++
@@ -1,9 +0,0 @@
-export function formatToKebabCase(string) {
-  const parsedString = String(string);
-
-  return parsedString
-    .toLowerCase()
-    .trim()
-    .replaceAll(' ', '-')
-    .replace(/[^0-9A-Za-z-]/g, '');
-}
