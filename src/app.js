fcaf07738daf0b55f6e282b553c34217010c545b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-05 16:09:15 -0300

    feat: add app.js

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import { join } from 'node:path';
+
+import express from 'express';
+
+import { forwardNotFoundError, handleError } from './controllers/error-handlers.js';
+import { index } from './routes/index.js';
+import { newMessage } from './routes/new-message.js';
+
+export const app = express();
+
+const { dirname } = import.meta;
+const viewsPath = join(dirname, 'views');
+
+app.set('views', viewsPath);
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', index);
+app.use('/new-message', newMessage);
+
+app.use(forwardNotFoundError);
+app.use(handleError);
