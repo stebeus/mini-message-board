77417bc443d457a2c34f488c9355b6499b62ff94
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-06 16:54:56 -0300

    feat: insert message to database on port

--- src/controllers/new-message.js
+++ src/controllers/new-message.js
@@ -1,7 +1,11 @@
+import { insertMessage } from '#root/models/queries.js';
+
 export const getNewMessage = (req, res) => {
 	res.render('new-message', { title: 'New message' });
 };
 
-export const postNewMessage = (req, res) => {
+export const postNewMessage = async (req, res) => {
+	const { username, message } = req.body;
+	await insertMessage(username, message);
 	res.redirect('/');
 };
